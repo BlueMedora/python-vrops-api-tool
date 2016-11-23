9387da2344a5c7e1024883ef7f0beb6e901b4aa7
Keith Stouffer <keith.stouffer@bluemedora.com>
2016-11-23 00:43:13 -0500

    added packaging script

--- make_dist.sh
+++ make_dist.sh
@@ -1,22 +1,101 @@
 #!/usr/bin/env bash
 echo "removing ./build and ./dist"
-rm -rf build dist suite-api-tool.dmg
+rm -rf build dist
 echo "creating app bundle"
 cd suite-api-tool && pyinstaller --onefile --windowed --distpath=../dist --workpath=../build ./suite-api-tool.spec && cd ..
 echo "removing extra suite-api-tool binary"
 rm ./dist/suite-api-tool
 echo "creating distribuatble disk image"
 
-disk_size=$(du -hs ./dist | cut -f1)
-last=${disk_size: -1}
-first=$((${disk_size:0:-1}+1))
-disk_size="$first$last"
+#the next chunk of code is taken from:
+# by Andy Maloney
+# http://asmaloney.com/2013/07/howto/packaging-a-mac-os-x-application-using-a-dmg/
 
-hdiutil create -size $disk_size -fs HFS+ -volname suite-api-tool ./dist
-mv dist.dmg suite-api-tool.dmg
-hdiutil unmount /Volumes/suite-api-tool
-hdiutil mount suite-api-tool.dmg
-cp -r ./dist/* /Volumes/suite-api-tool/
-echo "creating link to /Applications"
-ln -s /Volumes/suite-api-tool/Applications /Applications
-hdiutil unmount /Volumes/suite-api-tool
+# make sure we are in the correct dir when we double-click a .command file
+dir=${0%/*}
+if [ -d "$dir" ]; then
+  cd "$dir"
+fi
+
+# set up your app name, version number, and background image file name
+APP_NAME="suite-api-tool"
+VERSION="0.0.1"
+
+# you should not need to change these
+APP_EXE="${APP_NAME}.app/Contents/MacOS/${APP_NAME}"
+
+VOL_NAME="${APP_NAME} ${VERSION}"   # volume name will be "SuperCoolApp 1.0.0"
+DMG_TMP="${VOL_NAME}-temp.dmg"
+DMG_FINAL="${VOL_NAME}.dmg"         # final DMG name will be "SuperCoolApp 1.0.0.dmg"
+STAGING_DIR="./dist"             # we copy all our stuff into this dir
+
+rm -rf "${DMG_TMP}" "${DMG_FINAL}"
+
+
+# figure out how big our DMG needs to be
+#  assumes our contents are at least 1M!
+SIZE=`du -sh "${STAGING_DIR}" | sed 's/\([0-9\.]*\)M\(.*\)/\1/'` 
+SIZE=`echo "${SIZE} + 1.0" | bc | awk '{print int($1+0.5)}'`
+echo "$SIZE"
+if [ $? -ne 0 ]; then
+   echo "Error: Cannot compute size of staging dir"
+   exit
+fi
+
+# create the temp DMG file
+hdiutil create -srcfolder "${STAGING_DIR}" -volname "${VOL_NAME}" -fs HFS+ \
+      -fsargs "-c c=64,a=16,e=16" -format UDRW -size ${SIZE}M "${DMG_TMP}"
+
+echo "Created DMG: ${DMG_TMP}"
+
+# mount it and save the device
+DEVICE=$(hdiutil attach -readwrite -noverify "${DMG_TMP}" | \
+         egrep '^/dev/' | sed 1q | awk '{print $1}')
+
+sleep 2
+
+# add a link to the Applications dir
+echo "Add link to /Applications"
+pushd /Volumes/"${VOL_NAME}"
+ln -s /Applications
+popd
+
+# tell the Finder to resize the window, set the background,
+#  change the icon size, place the icons in the right position, etc.
+echo '
+   tell application "Finder"
+     tell disk "'${VOL_NAME}'"
+           open
+           set current view of container window to icon view
+           set toolbar visible of container window to false
+           set statusbar visible of container window to false
+           set the bounds of container window to {400, 100, 920, 440}
+           set viewOptions to the icon view options of container window
+           set arrangement of viewOptions to not arranged
+           set icon size of viewOptions to 72
+           set position of item "'${APP_NAME}'.app" of container window to {160, 205}
+           set position of item "Applications" of container window to {360, 205}
+           close
+           open
+           update without registering applications
+           delay 2
+     end tell
+   end tell
+' | osascript
+
+sync
+
+# unmount it
+hdiutil detach "${DEVICE}"
+
+# now make the final image a compressed disk image
+echo "Creating compressed image"
+hdiutil convert "${DMG_TMP}" -format UDZO -imagekey zlib-level=9 -o "${DMG_FINAL}"
+
+# clean up
+rm -rf "${DMG_TMP}"
+
+echo 'Done.'
+
+exit
+
